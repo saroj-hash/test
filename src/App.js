57e1de6f78442985c907f816a24aff5815bf73a6
BATMAN <tadim777@gmail.com>
2020-04-08 03:07:26 +0530

    v0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import Comp1 from "./components/comp1"
-export default class App extends React.Component{
-  render(){
-    return <Comp1/>
+import "bootstrap/dist/css/bootstrap.min.css"
+import React, { Component } from "react"
+import Container from "react-bootstrap/TabContainer"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import FormControl from "react-bootstrap/FormControl"
+import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom"
+import Userhome from "./components/comp1"
+import Subscribe from "./components/subscribe"
+// import NavDropdown from "react-bootstrap/NavDropdown"
+export default class App extends Component {
+  render() {
+    return <BrowserRouter>
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>Your Jaunt</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="nav-link" to="/">Explore</Link>
+              <Link className="nav-link" to="/subscribe">Subscribe</Link>
+            </Nav>
+            
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+      <Switch>
+        <Route exact path="/" component={Userhome}/>
+        <Route exact path="/subscribe" component={Subscribe}/>
+      </Switch>
+    </BrowserRouter>
   }
 }
---
+++ src/components/subscribe.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default class Subscribe extends React.Component {
+    render() {
+        return <div>
+            <h1>
+                Subscribe option coming soon!!
+        </h1>
+        </div>
+    }
+}
